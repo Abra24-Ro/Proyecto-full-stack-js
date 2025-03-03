04d0417b91ecc25e430aefbd6e2a47b33cc4e717
Abra24-Ro <robinsonabra24@gmail.com>
2025-03-03 16:45:56 -0500

    Fix: CORS issue and other improvements

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ dotenv.config();
 conectarDB();
 
 const dominiosPermitidos = [process.env.FRONTEND_URL];
-const corsOptions = {
-  origin: function name(origin, callback) {
-    if (dominiosPermitidos.indexOf(origin) !== -1) {
-      //El origen es permitido
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || dominiosPermitidos.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("No permitido por cors"));
+      callback(new Error("No permitido por CORS"));
     }
   },
+  credentials: true, // Permitir cookies si es necesario
 };
 
 app.use(cors(corsOptions));
+
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
